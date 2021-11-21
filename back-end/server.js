328aeec4bc0a94604779f69e89691272076ac270
Danielle Barker <caseybarker86@gmail.com>
2021-11-20 19:55:42 -0700

    another try to fix heroku

--- back-end/server.js
+++ back-end/server.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.static("../front-end/build"));
 app.use("/api/images", imageRouter);
 app.use('/', (req,res) =>{
-    res.sendFile(__dirname + "../front-end/build/index.html")
+    res.sendFile(__dirname + "/../front-end/build/index.html")
 })
 server.listen(port, () => {
     console.log(`Listening on port ${port}`);
